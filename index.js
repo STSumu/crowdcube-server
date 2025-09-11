60dc63c056efcfc2bfd78fffd3ecd6b5d7b3d294
STSumu <sabihatahsinsumu@gmail.com>
2025-09-11 18:31:12 +0600

    added campaign route

--- index.js
+++ index.js
@@ -1,45 +1,59 @@
-require('dotenv').config()
-const express=require('express')
-const cors=require('cors')
-const port=process.env.PORT || 3000;
+const express=require('express');
+const cors=require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port=process.env.PORT || 5000;
 const app=express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config();
 
-const username=process.env.DB_USER;
-const pass=process.env.DB_PASS;
-const connectionURI=`mongodb+srv://${username}:${pass}@cluster0.xzvxg9s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+app.use(cors());
+app.use(express.json());
 
-const client = new MongoClient(connectionURI,  {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    }
-);
-app.use(cors())
-app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send('crowdcube server')
-})
+const username = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const uri = `mongodb+srv://${username}:${pass}@learnmongo.4ifovjo.mongodb.net/?retryWrites=true&w=majority&appName=LearnMongo`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
-    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
+    const database = client.db("crowdcubeDb");
+    const campaignCollection = database.collection("campaignCollection");
+    const userCollection = database.collection("UserCollection");
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    console.log("Connected to MongoDB!");
+
+    
+    app.get("/campaigns", async (req, res) => {
+      const campaigns = await campaignCollection.find().toArray();
+      res.send(campaigns);
+    });
+    app.get("/campaigns/:campId",async(req,res)=>{
+      const {campId}=req.params;
+      const id=new ObjectId(campId);
+      const campaign=await campaignCollection.findOne({_id : id});
+      res.send(campaign);
+    })
+
+  } 
+  catch (error) {
+    console.error("DB Connection Error:", error);
   }
 }
+
 run().catch(console.dir);
 
+app.get('/', (req, res) => {
+  res.send('crowdcube server running');
+});
 
-app.listen(port,()=>{
-    console.log('server is running');
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
